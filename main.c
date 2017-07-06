c84009c51fc5158a3f3b6653a5fbe3262d6f576c
kobus <kobus.coetzeee@gmail.com>
2017-07-06 19:20:08 +0200

    working long

--- main.c
+++ main.c
@@ -12,7 +12,40 @@
 
 #include <stdio.h>
 #include <dirent.h>
+#include <sys/stat.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <stdbool.h>
 #include "libft.h"
+#include <time.h>
+
+void    test_stat(struct stat sb)
+{
+    char *time; 
+    //printf("Mode: %lo (octal)\n", (unsigned long) sb.st_mode);
+    // Size
+    printf("%ld\t", sb.st_size); 
+    // Last time modified
+    time = ctime(&sb.st_mtime);
+    time[ft_strlen(time) - 1] = '\0';
+    printf("%s ", time);
+
+}
+
+void    print_permissions(mode_t mode)
+{
+    printf(S_ISDIR(mode) ? "d" : "-");
+    printf((mode & S_IRUSR) ? "r" : "-");
+    printf((mode & S_IWUSR) ? "w" : "-");
+    printf((mode & S_IXUSR) ? "x" : "-");
+    printf((mode & S_IRGRP) ? "r" : "-");
+    printf((mode & S_IWGRP) ? "w" : "-");
+    printf((mode & S_IXGRP) ? "x" : "-");
+    printf((mode & S_IROTH) ? "r" : "-");
+    printf((mode & S_IWOTH) ? "w" : "-");
+    printf((mode & S_IXOTH) ? "x" : "-");
+}
+
 
 void	sort_list(struct dirent *list, int size)
 {
@@ -41,10 +74,42 @@ void	sort_list(struct dirent *list, int size)
 	}
 }
 
+void    display_default(struct dirent *list, int size)
+{
+    // Loop going through
+	while (size--)
+	{
+		if (list[size].d_name[0] != '.')
+		printf("%s\t", list[size].d_name);
+	}	
+}
+
+void    display_long_format(struct dirent *list, int size)
+{
+    int i;
+    struct stat sb;
+
+    i = 0;
+    while (i < size)
+    {
+        if (list[i].d_name[0] != '.')
+        {
+            stat(list[i].d_name, &sb);
+	        print_permissions(sb.st_mode);
+            printf("\t");
+            test_stat(sb);
+            printf("%s\t", list[i].d_name);
+        }
+        printf("\n");
+        i++;
+    }
+    printf("\n");
+}
+
 int	main(int argc, char **argv)
 {
 	struct dirent *list;
-	DIR *dir;
+    DIR *dir;
 	int len_list;
 	int i;
 
@@ -65,11 +130,9 @@ int	main(int argc, char **argv)
 	}
 	sort_list(list, len_list);
 	i = 0;
-	while (i < len_list)
-	{
-		if (list[i].d_name[0] != '.')
-		printf("%s\t", list[i].d_name);
-		i++;
-	}	
+
+    // Display everything
+    //display_default(list, len_list);
+    display_long_format(list, len_list);
 	return (0);
 }
