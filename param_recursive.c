646a6d8e0ca0468b6c6e0cd2c77154429b939e55
kobus <kobus.coetzeee@gmail.com>
2017-07-09 08:08:32 +0200

    almost done

--- param_recursive.c
+++ param_recursive.c
@@ -16,28 +16,48 @@ void display_recursive(char *dir, t_format *format)
         directories.path = (char*)malloc(sizeof(char) * (directories.max_len + 1));
         set_path(&directories, dir);
     }
-    
+   
     i = 0;
     list = load_list(dir, &len);
-    printf("%s:\n", dir);
-    while (i < len)
+    
+    if (format->has_l)
     {
-        if ((list[i].d_name[0] == '.' && format->has_a) || (list[i].d_name[0] != '.'))
-            printf("%s  ", list[i].d_name);
-        i++;
+        printf("%s: \n", dir);
+        //getchar();
+        display_long_format(list, len, format, dir);
+    }
+    else 
+    {
+        printf("%s:\n", dir);
+        while (i < len)
+        {
+            if ((list[i].d_name[0] == '.' && format->has_a) || (list[i].d_name[0] != '.'))
+                printf("%s  ", list[i].d_name);
+            i++;
+        }
     }
     
     i = 0;
-    printf("\n\n");
+    //printf("\n");
     while (i < len)
     {
+        /*if (list[i].d_type == DT_DIR)
+        {
+            printf("%s is a directory\n", list[i].d_name);
+            getchar();
+        }*/
         if (list[i].d_type == DT_DIR && ((list[i].d_name[0] == '.' && format->has_a) || (list[i].d_name[0] != '.')))
         {
-            push_path(&directories, list[i].d_name);
-            display_recursive(directories.path, format);
-            pop_path(&directories);                
+            if ((list[i].d_name[0] == '.' && list[i].d_name[1] == 0) || (list[i].d_name[0] == '.' && list[i].d_name[1] == '.'))
+                i++;
+            else
+            {
+                push_path(&directories, list[i].d_name);
+                display_recursive(directories.path, format);
+                pop_path(&directories);                
+            }
             //free(list);
         }
-          i++;
+        i++;
     }
 }
