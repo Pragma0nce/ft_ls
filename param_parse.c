5404ab0197a7a13343da2e86e0f9c232b83cfd59
Pragma0nce <kcoetzee@e4r6p4.jnb.42.fr>
2017-07-09 14:30:01 +0200

    normalized

--- param_parse.c
+++ param_parse.c
@@ -1,42 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   param_parse.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kcoetzee <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/07/09 14:26:37 by kcoetzee          #+#    #+#             */
+/*   Updated: 2017/07/09 14:28:15 by kcoetzee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_ls.h"
 
-t_option    *add_option(char opt, t_option *list)
+t_option	*add_option(char opt, t_option *list)
 {
-    t_option *temp;
-    temp = (t_option*)malloc(sizeof(t_option));
+	t_option *temp;
 
-    temp->option = opt;
-    temp->next = list;
-
-    return (temp);
+	temp = (t_option*)malloc(sizeof(t_option));
+	temp->option = opt;
+	temp->next = list;
+	return (temp);
 }
 
-void        print_options(t_option *list)
+void		print_options(t_option *list)
 {
-    while (list != NULL)
-    {
-        printf("Option added: %c\n", list->option);
-        list = list->next;
-    }
+	while (list != NULL)
+	{
+		printf("Option added: %c\n", list->option);
+		list = list->next;
+	}
 }
 
-t_path      *add_path(char *path_name, t_path *list)
+t_path		*add_path(char *path_name, t_path *list)
 {
-    t_path *temp;
-    temp = (t_path*)malloc(sizeof(t_path));
+	t_path *temp;
 
-    temp->path_name = path_name;
-    temp->next = list;
-
-    return (temp);
+	temp = (t_path*)malloc(sizeof(t_path));
+	temp->path_name = path_name;
+	temp->next = list;
+	return (temp);
 }
 
-
-void        print_paths(t_path *list)
+void		print_paths(t_path *list)
 {
-    while (list != NULL)
-    {
-        printf("Path added: %s\n", list->path_name);
-        list = list->next;
-    }
+	while (list != NULL)
+	{
+		printf("Path added: %s\n", list->path_name);
+		list = list->next;
+	}
 }
--- path.c
+++ path.c
@@ -1,53 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   path.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kcoetzee <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/07/09 14:28:49 by kcoetzee          #+#    #+#             */
+/*   Updated: 2017/07/09 14:29:46 by kcoetzee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_ls.h"
 
-void    set_path(t_directory *dir, char *str)
+void	set_path(t_directory *dir, char *str)
 {
-    if (ft_strlen(str) > dir->max_len)
-    {
-        free(dir->path);
-        dir->path = (char*)malloc(sizeof(char) * (ft_strlen(str) + 1));
-        dir->max_len = ft_strlen(str);
-    }
-
-    ft_strcpy(dir->path, str);
-    dir->path_len = ft_strlen(str);
+	if (ft_strlen(str) > dir->max_len)
+	{
+		free(dir->path);
+		dir->path = (char*)malloc(sizeof(char) * (ft_strlen(str) + 1));
+		dir->max_len = ft_strlen(str);
+	}
+	ft_strcpy(dir->path, str);
+	dir->path_len = ft_strlen(str);
 }
 
-void    push_path(t_directory *dir, char *str)
+void	push_path(t_directory *dir, char *str)
 {
-    char *temp;
+	char *temp;
 
-    if (dir->path_len + ft_strlen(str) >= dir->max_len)
-    {
-        temp = (char*)malloc(sizeof(char) * dir->max_len);
-        strcpy(temp, dir->path);
-        free(dir->path);
-        dir->path = (char*)malloc(sizeof(char) * (dir->max_len + ft_strlen(str) + 2));
-        dir->max_len = (dir->max_len + ft_strlen(str) + 1);
-        strcpy(dir->path, temp);
-        dir->path = ft_strjoin(dir->path, "/");
-        dir->path = ft_strjoin(dir->path, str);
-        dir->path_len = ft_strlen(dir->path);
-        free(temp);
-    }
-    else
-    {
-        dir->path = ft_strjoin(dir->path, "/");
-        dir->path = ft_strjoin(dir->path, str);
-        dir->path_len = ft_strlen(dir->path);
-    }
+	if (dir->path_len + ft_strlen(str) >= dir->max_len)
+	{
+		temp = (char*)malloc(sizeof(char) * dir->max_len);
+		strcpy(temp, dir->path);
+		free(dir->path);
+		dir->path = (char*)malloc(sizeof(char) *
+				(dir->max_len + ft_strlen(str) + 2));
+		dir->max_len = (dir->max_len + ft_strlen(str) + 1);
+		strcpy(dir->path, temp);
+		dir->path = ft_strjoin(dir->path, "/");
+		dir->path = ft_strjoin(dir->path, str);
+		dir->path_len = ft_strlen(dir->path);
+		free(temp);
+	}
+	else
+	{
+		dir->path = ft_strjoin(dir->path, "/");
+		dir->path = ft_strjoin(dir->path, str);
+		dir->path_len = ft_strlen(dir->path);
+	}
 }
 
-void    pop_path(t_directory *dir)
+void	pop_path(t_directory *dir)
 {
-    int i;
+	int i;
 
-    i = dir->path_len;
-    while (i > 0 && dir->path[i] != '/')
-    {
-        dir->path[i] = '\0';;
-        i--;
-    }
-    dir->path[i] = '\0';
-    dir->path_len = ft_strlen(dir->path);
+	i = dir->path_len;
+	while (i > 0 && dir->path[i] != '/')
+	{
+		dir->path[i] = '\0';
+		i--;
+	}
+	dir->path[i] = '\0';
+	dir->path_len = ft_strlen(dir->path);
 }
