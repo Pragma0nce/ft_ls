0248b7c7c4db2f04ac3f29b523bb0bf377254c63
kobus <kobus.coetzeee@gmail.com>
2017-07-09 07:12:39 +0200

    recursing working without extra parameters

--- param_recursive.c
+++ param_recursive.c
@@ -8,32 +8,36 @@ void display_recursive(char *dir, t_format *format)
     //static char *path;
     static t_directory directories;
 
+    //printf("dir to search: %s \n", dir);
+    //getchar();
     if (directories.path == NULL)
     {
         directories.max_len = 100;
         directories.path = (char*)malloc(sizeof(char) * (directories.max_len + 1));
         set_path(&directories, dir);
     }
-
-        i = 2;
-        list = load_list(dir, &len);
-        printf("%s:\n", dir);
-        while (i < len)
-        {
+    
+    i = 0;
+    list = load_list(dir, &len);
+    printf("%s:\n", dir);
+    while (i < len)
+    {
+        if ((list[i].d_name[0] == '.' && format->has_a) || (list[i].d_name[0] != '.'))
             printf("%s  ", list[i].d_name);
-            i++;
-        }
-        printf("\n\n");
-        i = 2;
-        while (i < len)
+        i++;
+    }
+    
+    i = 0;
+    printf("\n\n");
+    while (i < len)
+    {
+        if (list[i].d_type == DT_DIR && ((list[i].d_name[0] == '.' && format->has_a) || (list[i].d_name[0] != '.')))
         {
-            if (list[i].d_type == DT_DIR)
-            {
-                push_path(&directories, list[i].d_name);
-                display_recursive(directories.path, format);
-                pop_path(&directories);
-                //free(list);
-            }
-            i++;
+            push_path(&directories, list[i].d_name);
+            display_recursive(directories.path, format);
+            pop_path(&directories);                
+            //free(list);
         }
+          i++;
+    }
 }
